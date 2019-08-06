03297ad541c3613ed7ea5ad7192a960193eb7b03
Anssikka <Anssikka@gmail.com>
2019-08-06 17:50:35 +0300

    heroku database onkin casesensitive@

--- application/tournamentPackages/models.py
+++ application/tournamentPackages/models.py
@@ -42,10 +42,10 @@ class TournamentPackage(Base):
 
     @staticmethod
     def tournaments_bought_action_from(id):
-        stmt = text("SELECT bought_action_from_tournament.seller_name, tournament_package.tournament, tournament_package.buyin, bought_action_from_tournament.actionBoughtPct FROM account"
-                " left join bought_action_from_tournament ON account.id = bought_action_from_tournament.buyer_id"
-                " left join tournament_package on bought_action_from_tournament.tournament_package_id = tournament_package.id"
-                " where bought_action_from_tournament.buyer_id = {}".format(id))
+        stmt = text("SELECT bought_action_from_tournament.seller_name, tournament_package.tournament, tournament_package.buyIn, bought_action_from_tournament.actionBoughtPct FROM account"
+                " LEFT JOIN bought_action_from_tournament ON account.id = bought_action_from_tournament.buyer_id"
+                " LEFT JOIN tournament_package ON bought_action_from_tournament.tournament_package_id = tournament_package.id"
+                " WHERE bought_action_from_tournament.buyer_id = {}".format(id))
 
         res = db.engine.execute(stmt)
         asAnArray = []
